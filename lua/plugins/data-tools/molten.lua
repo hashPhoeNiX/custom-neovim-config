653ca6a0d1deca0ba93448907a050689b813115f
Oluwapelumi Adeosun <adeosun.pelumi5@gmail.com>
2025-10-08 01:04:36 +0100

    update molten

--- lua/plugins/data-tools/molten.lua
+++ lua/plugins/data-tools/molten.lua
@@ -96,7 +96,7 @@ return {
     init = function()
       --vim.g.python3_host_prog =
       --	vim.fn.expand("/etc/profiles/per-user/oluwapelumiadeosun/bin/myHomeModuleNvim-python3")
-      -- vim.g.molten_auto_open_output = true
+      vim.g.molten_auto_open_output = true
       vim.g.molten_image_provider = "image.nvim"
       -- vim.g.molten_output_win_max_height = 12
       -- vim.g.molten_output_win_hide_on_leave = false
