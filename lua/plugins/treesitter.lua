e828cfe8c0dfa0d583ebdb7a564c923d61734375
Oluwapelumi Adeosun <adeosun.pelumi5@gmail.com>
2025-12-17 13:44:09 +0100

    disable treesitter highlight

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
       ensure_installed = { "lua", "nix", "python" },
-      highlight = { enable = true },
-      indent = { enable = true }, 
+      highlight = { enable = false },
+      indent = { enable = true },
     })
   end
 }
